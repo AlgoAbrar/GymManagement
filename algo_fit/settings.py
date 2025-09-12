8baf175b7dddcaa7acb27a4f3530cb6ee70b239a
“Saiyedul <saiyedul.abrar@gmail.com>
2025-09-13 03:51:29 +0600

    “message”

--- algo_fit/settings.py
+++ algo_fit/settings.py
@@ -145,6 +145,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_FILES_DIR = BASE_DIR / 'static'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
